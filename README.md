dd1aa333fe37833e5ea3dfa707c33717d879967c
Atthar Mirza <atthar.mirza@gmail.com>
2024-08-03 10:02:33 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Data Robot AIde
+
+Query and analyze data from census and statistics offices around the world. Currently supported countries are:
+
+- India 🇮🇳, via the [Office of the Registrar General & Census Commissioner](https://censusindia.gov.in/census.website/data/census-tables)
+
+- Italy 🇮🇹, via the [Istituto Nazionale di Statistica](https://www.istat.it/en/data/databases/)
+
+- United States 🇺🇸, via the [U.S. Census Bureau](https://www.census.gov/)
+
+## Quick Start
+
+Information coming soon.
+
+## Installation
+
+Information coming soon.
---
+++ index.js
@@ -0,0 +1,22 @@
+import 'dotenv/config'
+import { GoogleGenerativeAI } from '@google/generative-ai'
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+
+/**
+ * Use this function to fetch and ask questions about data pulled from various 
+ * census and statistics APIs around the world.
+ * 
+ * @param {string} prompt User generated text to query an API
+ * @returns {string}
+ */
+async function main(prompt) {
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+
+    console.log(response.text());
+}
+
+main('hello world')
