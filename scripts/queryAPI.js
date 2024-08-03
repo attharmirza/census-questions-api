ebdc91a53a320c00f6a69761abe086afe5367db3
Atthar Mirza <atthar.mirza@gmail.com>
2024-08-03 18:07:21 -0400

    basic census query

--- scripts/queryAPI.js
+++ scripts/queryAPI.js
@@ -1,5 +1,17 @@
 import 'dotenv/config'
 
-export default async function queryAPI() {
+const testURL = `https://api.census.gov/data/2022/acs/acs1/spp?get=NAME&for=state:*&key=${process.env.US_CENSUS_API_KEY}`
 
+export default async function queryAPI(url) {
+    try {
+        const response = await fetch(url)
+
+        const json = await response.json()
+
+        console.log(json)
+    } catch (error) {
+        console.error(error)
+    }
 }
+
+queryAPI(testURL)
