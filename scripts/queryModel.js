58d01df706a847a27094da50936b6616ac4a31cb
Atthar Mirza <atthar.mirza@gmail.com>
2024-08-09 15:27:56 -0400

    refactor and bug fix

--- scripts/queryModel.js
+++ scripts/queryModel.js
@@ -11,31 +11,57 @@ import { csvParse, groups, ascending } from 'd3'
 export async function generateFunctionCall() {
     let censusGroups, geographyCounties
 
+    /**
+     * Setting up the censusGroup parameter in the function call
+     */
     try {
         const hostname = 'api.census.gov'
         const pathnameCommon = 'data/2022/acs/acs1/'
 
         censusGroups = await queryAPI(hostname, `${pathnameCommon}groups.json`)
+    } catch (err) {
+        throw err
+    }
 
+    /**
+     * I can filter some of the groups down to make the query more efficient,
+     * and to make it use less tokens, based on a better understanding of Table
+     * IDs. See more here: 
+     * https://www.census.gov/programs-surveys/acs/data/data-tables/table-ids-explained.html
+     */
+    censusGroups = censusGroups.groups
+        .filter(f => !f.name === false && !f.description === false)
+        .filter(f => f.name.slice(0, 1) === 'B')
+        .sort((a, b) => ascending(a.name, b.name))
+
+    const censusGroupsDescription = `This has to be one group ID from the following list of group ID values and the description of data each represents: ${censusGroups.map(d => `${d.name} = ${d.description}`).join(', ')}.`
+
+    /**
+     * Setting up the censusGeography function parameter in the function call    
+     */
+
+    try {
         geographyCounties = await fs.readFile('data/county_fips_master.csv', { encoding: 'utf-8' })
     } catch (err) {
         throw err
     }
 
-    censusGroups = censusGroups.groups
-        .filter(f => !f.name === false && !f.description === false)
-        .sort((a, b) => ascending(a.name, b.name))
-
     geographyCounties = groups(csvParse(geographyCounties), d => d.state_name).map(d => ({
         state_name: d[0],
-        fips: d[1][0].state,
+        fips: (d[1][0].state.length === 1) ? `0${d[1][0].state}` : d[1][0].state, // Need to add back leading zeroes for FIPS codes. Should probably edit source dataset.
         counties: d[1].map(e => ({
             county_name: e.county_name,
             fips: e.fips
         }))
     }))
 
-    const functionCall = {
+    const geographyCountiesDescription = `For statistics and data covering national level data in the United States, the value is always us:1. For statistics and data covering state level data for states within the United States. Value is "state:" followed by a comma separated list of relevant state FIPS codes. The FIPS code for each state are as follows: ${geographyCounties.map(d => `${d.state_name} = ${d.fips}`).join(', ')}.`
+
+    /**
+     * Finally, assembling and returning the function call
+     */
+
+    return {
         name: 'get_data_from_us_census_american_community_survey_1_year_2022',
         description: 'Get a combination of demographic data from the U.S. Census Bureau\'s American Community Survey 1 Year dataset, including things like age, race, sex, household income, ancestry, education and so on.',
         parameters: {
@@ -43,18 +69,16 @@ export async function generateFunctionCall() {
             properties: {
                 censusGroup: {
                     type: 'STRING',
-                    description: `This has to be one group ID from the following list of group ID values and the description of data each represents: ${censusGroups.map(d => `${d.name} = ${d.description}`).join(', ')}.`
+                    description: censusGroupsDescription
                 },
                 censusGeography: {
                     type: 'STRING',
-                    description: `For statistics and data covering national level data in the United States, the value is always us:1. For statistics and data covering state level data for states within the United States. Value is "state:" followed by a comma separated list of relevant state FIPS codes. The FIPS code for each state are as follows: ${geographyCounties.map(d => `${d.state_name} = ${d.fips}`).join(', ')}.`
+                    description: geographyCountiesDescription
                 }
             },
             required: ["censusGroup", "censusGeography"]
         }
     }
-
-    return functionCall
 }
 
 /**
